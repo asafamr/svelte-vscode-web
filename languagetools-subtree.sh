cae38c83a96f4ea6fa7f6768365e9d9e7959a49a
AsafA <asaf.amrami@gmail.com>
2021-10-08 15:56:37 +0300

    copied contrubtions from original

--- languagetools-subtree.sh
+++ languagetools-subtree.sh
@@ -1,1 +1,5 @@
-git subtree add --prefix vendored/langauge-tools https://github.com/sveltejs/language-tools.git master --squash
+# inital was
+# git subtree add --prefix vendored/langauge-tools https://github.com/sveltejs/language-tools.git master --squash
+git subtree pull --prefix vendored/langauge-tools https://github.com/sveltejs/language-tools.git master --squash
+
+
